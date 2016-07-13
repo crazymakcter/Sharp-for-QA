e0629a4356abad3f2a7d2ee51bb9e196ff047afa
crazymakcter <crazymakcter@gmail.com>
2016-07-13 19:03:49 +0300

    Add 06-03 and 07-01
    07-01 Inprogress

---
+++ Main/Lesson06-03/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson06_03
+{
+    /// <summary>
+    /// 1. Создать прямоугльный двумнрый массив случайных чисел 10 на 10
+    /// 2. Нати суммы эленемтов массива, расположенных по диагонали:
+    /// 3. Обеспечить вывод на консоль массива и найденных сумм диагональных
+    /// элементов
+    /// </summary>
+    class Program
+    {
+        
+        static public int[,] CreateAndFillArray()
+        {
+            Random rand_val = new Random();
+            Console.WriteLine("Create array [10,10] with random value");
+            int[,] array = new int[10,10];
+            for(int i = 0;  i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    array[i, j] = rand_val.Next(0, 100);
+                    Console.Write(array[i,j] + "\t");
+                }
+                Console.Write('\n');
+
+            }
+            return array;
+        }
+
+        static public void FoundSummDiagonalsArray(int[,] array_summ)
+        {
+            int summ = 0;
+            for(int i = 0;  i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    if ((i == j))
+                    {
+                        //Console.WriteLine(summ);
+                        summ = summ + array_summ[i, j];
+                        //Console.Write(array_summ[i, j] + " ");
+                    }
+                    if((i + j) == 9)
+                    {
+                        summ = summ + array_summ[i,j];
+                    }
+                    
+                }
+                
+
+            }
+            Console.Write("Summ: ");
+            Console.Write(summ);
+        }
+            
+        static void Main(string[] args)
+        {
+
+            FoundSummDiagonalsArray(CreateAndFillArray());
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Main/Lesson07-01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessom07_01
+{
+    class Program
+    {
+        interface IMoveable
+        {   
+          double Move(int range);
+        }
+
+        class Transport : IMoveable
+        {
+            public int speed{get;set;}
+            public int petrol{get;set;}
+            protected int fuelConsumptionPerMile;
+            public double Move(int range)
+            {
+                return range / speed;
+            }
+            public void AddPetrol(int size)
+            {
+                petrol = petrol + size;
+            }      
+
+        }
+
+        class SportCar : Transport
+        {
+            public bool Turbo;
+            override public double Move(int range)
+            {
+                if (Turbo == true)
+                    {
+                        return range / (speed * 1.5);
+                    }
+                else
+                {
+                    return range / speed;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SportCar car = new SportCar();
+            
+            car.Turbo = true;
+            
+            Console.Write(car.Move(500));
+        }
+    }
+}
