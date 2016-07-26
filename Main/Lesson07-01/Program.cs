f24c7b082940098d0a2a523f6f1f5edbef5bdf98
Maksym Vertebnyi <m.vertebnyi@plarium.local>
2016-07-26 17:32:28 +0300

    Update task

--- Main/Lesson07-01/Program.cs
+++ Main/Lesson07-01/Program.cs
@@ -15,46 +15,130 @@ namespace Lessom07_01
 
         class Transport : IMoveable
         {
-            public int speed{get;set;}
-            public int petrol{get;set;}
-            protected int fuelConsumptionPerMile;
-            virtual public double Move(int range)
+            public double petrolTank{get;set;}
+            public double speed{get;set;}
+            public double fuelConsumptionPerMile;
+            protected double turboBoost = 0;
+            public virtual double Move(int range)
             {
                 return range / speed;
             }
-            public void AddPetrol(int size)
+            public virtual void AddPetrol(int size)
             {
-                petrol = petrol + size;
-            }      
+              
+            }
+
+            public virtual bool DistanceToStopCar(int distance, bool turbo)
+          {
+              if (turbo == true)
+              {
+                turboBoost = 0.5;
+              }
+              else
+              {
+                turboBoost = 0.0;
+              }
+
+            if (distance > (petrolTank*fuelConsumptionPerMile*turboBoost))
+            {
+              return false;
+            }
+            else
+            {
+              return true;
+            }
+          }
+
 
         }
 
         class SportCar : Transport
         {
+            private double petrolTank = 43.0;
+            private double petrol = 0.0;
             public bool Turbo;
-            override public double Move(int range)
+            private double time = 0;
+            private double fuelConsumptionPerMile = 8.00;
+            //public int distanceToStop;
+
+            public override double Move(int range)
             {
-                if (Turbo == true)
-                    {
-                        return range / (speed * 1.5);
-                    }
+
+              if (Turbo == true)
+              {
+                time = range / (speed*1.5);
+                if (DistanceToStopCar(range, true) == true)
+                {
+                  return time;
+                }
                 else
                 {
-                    return range / speed;
+                  Console.WriteLine("Please add fuel for travelling");
+                  return 0;
                 }
+              }
+              else
+              {
+                time = range/speed;
+                if (DistanceToStopCar(range, false) == true)
+                {
+                  return time;
+                }
+                else
+                {
+                  Console.WriteLine("Please add fuel for travelling");
+                  return 0;
+                }
+              }
             }
+
+          public override void AddPetrol(int size)
+          {
+            if (petrolTank <= (petrol + size))
+            {
+              Console.WriteLine("PetrolTank is smaller than size petrol for refueling");
+            }
+            else
+            {
+              petrolTank = petrol + size;
+            }
+          }
+
+          public override bool DistanceToStopCar(int distance, bool turbo)
+          {
+            double res = 0.0;
+            if (turbo == true)
+            {
+              turboBoost = 0.5;
+            }
+            else
+            {
+              turboBoost = 1.0;
+            }
+
+            res = (petrolTank*fuelConsumptionPerMile)*turboBoost;
+            if (distance > res)
+            {
+              return false;
+            }
+            else
+            {
+              return true;
+            }
+          }
         }
 
         static void Main(string[] args)
         {
+
             SportCar car = new SportCar();
-            
+            car.AddPetrol(42);
             car.Turbo = false;
             car.speed = 120;
-            Console.Write("Turbo=fale, Car spend {0} on {1} km \n", car.Move(500), car.speed);
+            Console.Write("Turbo=false, Car spend {0} on {1} km \n", car.Move(200), car.speed);
             car.Turbo = true;
             car.speed = 120;
-            Console.Write("Turbo=true, Car spend {0} on {1} km \n", car.Move(500), car.speed);
+            Console.Write("Turbo=true, Car spend {0} on {1} km \n", car.Move(200), car.speed);
             Console.ReadKey();
         }
     }
